6a2ec840a4bcbe350ac38d4366004b3d7891386f
voldelord <voldelord26@gmail.com>
2024-01-10 09:10:15 -0400

    delete admin template

--- frontend/marketplace-stores/admintemplatemarketplace-Felicia/components/usuarios/Container.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Container = ({ children }) => {
-  return (
-    <div className="bg-white shadow-md p-3 rounded-lg w-full">{children}</div>
-  );
-};
-
-export default Container;
